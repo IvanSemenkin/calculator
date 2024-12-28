0bfad24dec18ee4d6351085752807d38f1b8d3af
IvanSemenkin <ismenkin@gmail.com>
2024-12-28 11:49:44 +0300

    Добавлено деление

--- js/app.js
+++ js/app.js
@@ -4,26 +4,32 @@ const submitButton = document.getElementById("submit");
 const resultElement = document.getElementById("result");
 const btnPlus = document.getElementById("plus");
 const btnMinus = document.getElementById("minus");
-const btnMultiply = document.getElementById("multiply")
+const btnMultiply = document.getElementById("multiply");
+const btnDivision = document.getElementById("division");
 const btnClear = document.getElementById("clear");
 
 let action = "";
 
 btnPlus.onclick = function () {
-    action = "plus";
+  action = "plus";
 };
 
 btnMinus.onclick = function () {
-    action = "minus";
+  action = "minus";
 };
 
 btnMultiply.onclick = function () {
-    action = "multiply";
+  action = "multiply";
 };
 
+btnDivision.onclick = function () {
+  action = "division";
+};
 
 function printResult(result) {
-  if (result < 0) {
+  if (typeof result === "string") {
+    resultElement.style.color = "red";
+  } else if (result < 0) {
     resultElement.style.color = "red";
   } else {
     resultElement.style.color = "green";
@@ -35,6 +41,7 @@ function clearInputs() {
   inputOne.value = "";
   inputTwo.value = "";
   resultElement.innerHTML = "";
+  action = "";
 }
 
 btnClear.onclick = function () {
@@ -45,23 +52,35 @@ function computeResult(inp1, inp2, actionSymbol) {
   const num1 = Number(inp1.value);
   const num2 = Number(inp2.value);
   let result;
-  
+
   if (actionSymbol === "plus") {
     result = num1 + num2;
   } else if (actionSymbol === "minus") {
     result = num1 - num2;
   } else if (actionSymbol === "multiply") {
-    result = num1 * num2
+    result = num1 * num2;
+  } else if (actionSymbol === "division") {
+    if (num2 === 0) {
+      result = "Cannot divide by zero";
+      resultElement.style.color = "red";
+      resultElement.textContent = result;
+      return result;
+    } else {
+      result = num1 / num2;
+    }
   }
-  
   printResult(result);
 }
 
-
 submitButton.onclick = function () {
-    if (action === "plus" || action === "minus" || action === "multiply") {
-      computeResult(inputOne, inputTwo, action);
-    } else {
-      resultElement.innerHTML = "Please select an operation";
-    }
-  };
+  if (
+    action === "plus" ||
+    action === "minus" ||
+    action === "multiply" ||
+    action === "division"
+  ) {
+    computeResult(inputOne, inputTwo, action);
+  } else {
+    resultElement.innerHTML = "Please select an operation";
+  }
+};
